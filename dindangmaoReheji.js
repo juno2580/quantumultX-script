666e4148e926b62465a9ec6f78c5dc6d108c16e6
juno2580 <131781194+juno2580@users.noreply.github.com>
2023-11-19 17:00:05 +0800

    叮当猫Revenuecat 系列解锁合集

--- dindangmaoReheji.js
+++ dindangmaoReheji.js
@@ -10,8 +10,8 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/juno2580/quantumultX-script/main/dindangmaoReheji.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/juno2580/quantumultX-script/main/dindangmaoReheji.js
 ^https:\/\/app-measurement\.com\/config\/app\/.+ url reject
 
 [mitm]
